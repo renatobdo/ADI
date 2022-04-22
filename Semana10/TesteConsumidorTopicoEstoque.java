87e83c49a0859c1577f97fbb123b5b257c903d7a
Renato Bueno <renatobdo@gmail.com>
2022-04-22 15:42:18 -0300

    Create TesteConsumidorTopicoEstoque.java

---
+++ Semana10/TesteConsumidorTopicoEstoque.java
@@ -0,0 +1,56 @@
+package br.com.ifsp.jms;
+import java.util.Scanner;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import javax.naming.InitialContext;
+
+//Consumidor agora é durável
+public class TesteConsumidorTopicoEstoque {
+
+    public static void main(String[] args) throws Exception {
+
+        InitialContext context = new InitialContext();
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+
+        Connection connection = factory.createConnection(); 
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+//Repare que as linhas 28 mudou o que está na linha 29 e o que está na linha 30 mudou para o que está na linha 31:
+        //Destination topico = (Destination) context.lookup("loja");
+        Topic topico = (Topic) context.lookup("loja");
+        //MessageConsumer consumer = session.createConsumer(topico);
+        MessageConsumer consumer = session.createDurableSubscriber(topico, "assinaturaDoEstoque");
+
+        consumer.setMessageListener(new MessageListener() {
+
+            @Override
+            public void onMessage(Message message) {
+
+                TextMessage textMessage = (TextMessage)message;
+
+                try {
+                    System.out.println(textMessage.getText());
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+
+        new Scanner(System.in).nextLine();
+
+        session.close();
+        connection.close();
+        context.close();
+    }
+}
