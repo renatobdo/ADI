2c6ac95920acff507991a5035608967b3508935b
Renato Bueno <renatobdo@gmail.com>
2022-04-22 11:57:00 -0300

    Create TesteProdutor2.java

---
+++ Semana10/TesteProdutor2.java
@@ -0,0 +1,41 @@
+package br.com.ifsp.jms;
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+
+public class TesteProdutor2 {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws Exception {
+		
+		InitialContext context = new InitialContext();
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+
+		Connection connection = factory.createConnection();
+		connection.start();
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+		Destination fila = (Destination) context.lookup("financeiro");
+		MessageProducer producer = session.createProducer(fila);
+		
+		for(int i = 0; i < 1000; i ++) { 
+		    Message message = session.createTextMessage("<pedido><id>" + i + "</id></pedido>");
+		    producer.send(message);
+		}
+
+		session.close();
+		connection.close();
+		context.close();
+	}
+}
