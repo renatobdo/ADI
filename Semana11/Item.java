472f0e1748fc592008fee6fe30410f379cbb56b9
Renato Bueno <renatobdo@gmail.com>
2022-04-29 10:08:18 -0300

    Create Item.java

---
+++ Semana11/Item.java
@@ -0,0 +1,62 @@
+package br.com.ifsp.jms;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Item implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+
+	Item() {
+	}
+
+	public Item(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", nome=" + nome + "]";
+	}
+
+}
