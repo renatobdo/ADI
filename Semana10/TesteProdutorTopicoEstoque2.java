831136cf3e223bee7e0133fa75cf4734b7c62655
Renato Bueno <renatobdo@gmail.com>
2022-04-29 01:05:20 -0300

    Create TesteProdutorTopicoEstoque2.java

---
+++ Semana10/TesteProdutorTopicoEstoque2.java
@@ -0,0 +1,37 @@
+package br.com.ifsp.jms;
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+public class TesteProdutorTopicoEstoque2 {
+
+    public static void main(String[] args) throws Exception {
+
+        InitialContext context = new InitialContext();
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+
+        Connection connection = factory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Destination topico = (Destination) context.lookup("loja");
+        MessageProducer producer = session.createProducer(topico);
+        
+        Message message = session.createTextMessage("<pedido><id>222</id></pedido>");
+        message.setBooleanProperty("ebook", true);
+        producer.send(message);
+
+        session.close();
+        connection.close();
+        context.close();
+    }
+}
