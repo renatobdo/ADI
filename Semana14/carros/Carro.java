7a4a4de7381db2189a68d5ca23640649410d124a
Renato Bueno <renatobdo@gmail.com>
2022-05-20 17:06:15 -0300

    Create Carro.java

---
+++ Semana14/carros/Carro.java
@@ -0,0 +1,26 @@
+package br.com.ifsp.carros;
+
+public class Carro {
+	private Long id;
+	private String nome;
+	
+	
+	public Carro(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
