3722896f1e5fc23507fd6b3b49ec054e1730bb35
Renato Bueno <renatobdo@gmail.com>
2022-08-31 21:51:42 -0300

    Update LeaderElection.java

--- Semana5/LeaderElection.java
+++ Semana5/LeaderElection.java
@@ -1,4 +1,5 @@
-package leader.election;
+package election;
+
 /*
  *  MIT License
  *
@@ -30,27 +31,69 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Zookeeper Client Threading Model & Zookeeper Java API
+ * Leader Election Implementation in Java
+ * https://www.udemy.com/course/distributed-systems-cloud-computing-with-java/
  */
 public class LeaderElection implements Watcher {
     private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+    //O servidor do zookeeper fica constantemente verificando a conexão com o cliente
+    //Se o servidor zookeeper não ouvir nada do cliente em um tempo maior que o definido
+    // no timeout 3000ms irá considerar o cliente desconectado ou morto
     private static final int SESSION_TIMEOUT = 3000;
+    private static final String ELECTION_NAMESPACE = "/election";
     private ZooKeeper zooKeeper;
+    private String currentZnodeName;
 
+    // NOTE - Don't forget to create the /election ZNode
     public static void main(String[] arg) throws IOException, InterruptedException, KeeperException {
         LeaderElection leaderElection = new LeaderElection();
 
         leaderElection.connectToZookeeper();
+        leaderElection.volunteerForLeadership();
+        leaderElection.electLeader();
         leaderElection.run();
         leaderElection.close();
         System.out.println("Disconnected from Zookeeper, exiting application");
     }
 
+    public void volunteerForLeadership() throws KeeperException, InterruptedException {
+    	//o prefixo c é de candidato
+        String znodePrefix = ELECTION_NAMESPACE + "/c_";
+        // o segundo parâmetro é o dado que queremos colocar, o outro parâmetro quer dizer que
+        //se desconectarmos do zookeeper o znode será deletado. O nó do znode será 
+        String znodeFullPath = zooKeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+
+        System.out.println("znode name " + znodeFullPath);
+        this.currentZnodeName = znodeFullPath.replace("/election/", "");
+    }
+
+    public void electLeader() throws KeeperException, InterruptedException {
+        List<String> children = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+
+        Collections.sort(children);
+        //o nome da sequência do nó mais curto será o primeiro elemento
+        String smallestChild = children.get(0);
+
+        //Verificando se sou ou não o líder com uma comparação simples
+        if (smallestChild.equals(currentZnodeName)) {
+            System.out.println("I am the leader");
+            return;
+        }
+
+        System.out.println("I am not the leader, " + smallestChild + " is the leader");
+    }
+
     public void connectToZookeeper() throws IOException {
+    	// o terceiro parâmetro é um objeto do tipo watcher
+    	// A API do zookeeper é baseada em evento e assíncrona.
+    	//Para ser notificado sobre conexão e desconexão é necessário registrar
+    	// um event handler e passar ao zookeeper como um objeto watcher
         this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
     }
 
     private void run() throws InterruptedException {
+    	//Coloca a thread principal em um estado de espera;
+    	// Precisa disso para podermos visualizar os outros prints como o de conexão estabelecida
         synchronized (zooKeeper) {
             zooKeeper.wait();
         }
@@ -60,7 +103,9 @@ public class LeaderElection implements Watcher {
         this.zooKeeper.close();
     }
 
+    @Override
     public void process(WatchedEvent event) {
+    	//Recebendo um evento e verificando que tipo de evento é
         switch (event.getType()) {
             case None:
                 if (event.getState() == Event.KeeperState.SyncConnected) {
@@ -68,6 +113,7 @@ public class LeaderElection implements Watcher {
                 } else {
                     synchronized (zooKeeper) {
                         System.out.println("Disconnected from Zookeeper event");
+                        //o método notifyAll chama o método close que encerra a conexão
                         zooKeeper.notifyAll();
                     }
                 }
